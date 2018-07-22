b3a14f2295e85f6b6e55fcb9ca2c7f30f18848b9
Walter Erquinigo <wallace@fb.com>
2018-07-22 11:28:38 -0700

    ortografia

--- src/pages/home.js
+++ src/pages/home.js
@@ -12,10 +12,10 @@ export default class Home extends React.Component {
         return (
             <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
                 <div key={"text1"} style={{ height: "50%", width: "50%", color: "green", textAlign: "center", paddingLeft: "3em" }}>
-                    <h1 style={{ fontVariant: "small-caps", color: "#22264F" }}> Bienvenidos Ciudadanos! </h1>
-                    <h2> Les presentamos a <span style={{ fontVariant: "small-caps", fontWeight: "700" }}>"CongresMan!"</span> el nuevo héroe democrático que te enseñara la importancia de particiar en las
+                    <h1 style={{ fontVariant: "small-caps", color: "#22264F" }}> ¡Bienvenidos Ciudadanos! </h1>
+                    <h2> Les presentamos a <span style={{ fontVariant: "small-caps", fontWeight: "700" }}>"CongresMan"</span>, el nuevo héroe democrático que te enseñará la importancia de participar en las
                         labores legislativas del congreso. </h2> <br /> <br />
-                    <h1> Enterate como puedes empoderarte! </h1> </div>
+                    <h1> ¡Entérate cómo puedes empoderarte! </h1> </div>
                 <div key={"text2"} style={{ height: "40%", width: "40%", textAlign: "center", paddingTop: "5em" }}>
                     <div key={"text3"}  onClick={(e) => {
                         () => {console.log("ee"); window.location = 'https://www.facebook.com/CongresMan-2054574541523427/'}
